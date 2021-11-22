4e6862c02d27eb9730d3f4585e0fe0d1961f5de1
eastriverlee <dan0720@naver.com>
2021-11-23 02:10:13 +0900

    fix issues

--- Sources/hocus/logic.swift
+++ Sources/hocus/logic.swift
@@ -1,32 +1,47 @@
 import Cocoa
 import AppKit
 
-func getAllWindows(in screen: Screen) -> [Window] {
+extension Window {
+    func isPrior(to window: Window) -> Bool {
+        self.position.x < window.position.x ||
+        (self.position.x == window.position.x && self.position.y < window.position.y) ||
+        (self.position.x == window.position.x && self.position.y == window.position.y && self.index < self.index)
+    }
+}
+
+func getAllWindows(in screen: Screen? = nil) -> [Window] {
     let options = CGWindowListOption(arrayLiteral: .excludeDesktopElements, .optionOnScreenOnly)
     let windowsListInfo = CGWindowListCopyWindowInfo(options, CGWindowID(0))
     let infoList = windowsListInfo as! [[String:Any]]
-    let visibleWindows = infoList.filter {
-        $0["kCGWindowLayer"] as! Int == 0
-    }.map {
-        $0["kCGWindowOwnerPID"] as! Int32
+    var visibleWindows = infoList.filter { $0["kCGWindowLayer"] as! Int == 0 }.map { window in
+        window["kCGWindowOwnerPID"] as! Int32
     }
-    let pids = Array(Set(visibleWindows))
-    let applications = pids.map { Application($0) }
-    var windows = applications.flatMap { $0.windows }.filter { window in window.screen == screen.index }
-    if let fullScreen = windows.first(where:{ $0.isFull }) {
-        return [fullScreen]
+    visibleWindows = Array(Set(visibleWindows))
+    let applications = visibleWindows.map { pid in Application(pid) }
+    var windows = applications.flatMap { $0.windows }
+    if let screen = screen {
+        windows = windows.filter { window in window.screen == screen.index }
+        if let fullScreen = windows.first(where:{ $0.isFull }) {
+            return [fullScreen]
+        }
     }
     windows.sort { (first, second) in
-        let origin = [first.position, second.position]
-        return origin[0].x < origin[1].x || (origin[0].x == origin[1].x && origin[0].y < origin[1].y)
+        return first.isPrior(to: second)
     }
     return windows
 }
 
-class Screen: CustomStringConvertible {
-    private let screen: NSScreen
-    let description: String
+class UI: CustomStringConvertible {
     let index: Int
+    let description: String
+    init(_ i: Int, _ description: String) {
+        index = i
+        self.description = description
+    }
+}
+
+class Screen: UI {
+    private let screen: NSScreen
     var frame: CGRect { screen.frame }
     var position: CGPoint { frame.origin }
     var size: CGSize { CGSize(width: frame.width, height: frame.height) }
@@ -34,8 +49,7 @@ class Screen: CustomStringConvertible {
 
     init(_ screen: NSScreen, _ i: Int) {
         self.screen = screen
-        description = screen.localizedName
-        index = i
+        super.init(i, screen.localizedName)
     }
     @discardableResult
     func next() -> Screen {
@@ -44,10 +58,6 @@ class Screen: CustomStringConvertible {
         let screen = screens[next]
 
         screen.windows.first!.focus()
-        print("switched to \(screen)")
-        for window in screen.windows {
-            print(window)
-        }
         return screen
     }
     @discardableResult
@@ -57,18 +67,13 @@ class Screen: CustomStringConvertible {
         let screen = screens[previous]
 
         screen.windows.first!.focus()
-        print("switched to \(screen)")
-        for window in screen.windows {
-            print(window)
-        }
         return screen
     }
     @discardableResult
     func focusNext() -> Window {
         let windows = self.windows
-        let last = windows.count - 1
         let focus = windows.firstIndex{ $0.isFocused }!
-        let window = windows[focus < last ? focus + 1 : 0]
+        let window = windows[focus < windows.count - 1 ? focus + 1 : 0]
 
         window.focus()
         return window
@@ -86,41 +91,43 @@ class Screen: CustomStringConvertible {
 
 class Application: CustomStringConvertible {
     let pid: Int32
-    let application: NSRunningApplication
+    let interface: AXUIElement
     let description: String
     var windows: [Window] = []
     var isActive: Bool {
-        application.isActive
+        var isActive: CFTypeRef?
+
+        AXUIElementCopyAttributeValue(interface, kAXFrontmostAttribute as CFString, &isActive)
+        return isActive as! Bool
     }
 
     init(_ pid: Int32) {
         var windows: CFArray?
-        let application = AXUIElementCreateApplication(pid)
+        var name: CFTypeRef?
 
         self.pid = pid
-        self.application = NSWorkspace.shared.runningApplications.first { $0.processIdentifier == pid }!
-        self.description = self.application.localizedName ?? ""
-        AXUIElementCopyAttributeValues(application, kAXWindowsAttribute as CFString, 0, Int.max, &windows)
+        interface = AXUIElementCreateApplication(pid)
+        AXUIElementCopyAttributeValue(interface, kAXTitleAttribute as CFString, &name)
+        description = name as! String
+        AXUIElementCopyAttributeValues(interface, kAXWindowsAttribute as CFString, 0, Int.max, &windows)
         if let windows = windows as? [AXUIElement] {
-            self.windows = windows.map { Window(self, $0) }
+            self.windows = windows.enumerated().map { (i, window) in Window(self, window, i) }
         }
     }
     func activate() {
-        if !isActive {
-            application.activate(options: [.activateIgnoringOtherApps])
-        }
+        AXUIElementSetAttributeValue(interface, kAXFrontmostAttribute as CFString, kCFBooleanTrue)
     }
 }
 
-class Window: CustomStringConvertible {
+class Window: UI {
     let application: Application
-    var window: AXUIElement
+    var interface: AXUIElement
     var position: CGPoint = .zero
     var size: CGSize = .zero
     var isMain: Bool {
         var isMain: CFTypeRef?
 
-        AXUIElementCopyAttributeValue(window, kAXMainAttribute as CFString, &isMain)
+        AXUIElementCopyAttributeValue(interface, kAXMainAttribute as CFString, &isMain)
         return isMain as! Bool
     }
     var isFull: Bool {
@@ -130,31 +137,30 @@ class Window: CustomStringConvertible {
     var screen: Int {
         screens.firstIndex { NSPointInRect(position as NSPoint, $0.frame) }!
     }
-    var description: String {
-        application.description
-    }
 
-    init(_ application: Application, _ window: AXUIElement) {
+    init(_ application: Application, _ window: AXUIElement, _ i: Int) {
         var position: CFTypeRef?
         var size: CFTypeRef?
 
         self.application = application
-        self.window = window
-        AXUIElementCopyAttributeValue(window, kAXPositionAttribute as CFString, &position)
+        self.interface = window
+        AXUIElementCopyAttributeValue(interface, kAXPositionAttribute as CFString, &position)
         AXValueGetValue(position as! AXValue, .cgPoint, &self.position)
-        AXUIElementCopyAttributeValue(window, kAXSizeAttribute as CFString, &size)
+        AXUIElementCopyAttributeValue(interface, kAXSizeAttribute as CFString, &size)
         AXValueGetValue(size as! AXValue, .cgSize, &self.size)
+        super.init(i, application.description + ":\(self.position)")
     }
     func focus() {
         application.activate()
-        AXUIElementPerformAction(self.window, kAXRaiseAction as CFString)
+        while !application.isActive { }
+        AXUIElementSetAttributeValue(interface, kAXMainAttribute as CFString, kCFBooleanTrue)
         print(self)
     }
 }
 
 var screens: [Screen] {
     NSScreen.screens.sorted { (first, second) in
-        let origin = [first.frame.origin, second.frame.origin]
-        return origin[0].x < origin[1].x || (origin[0].x == origin[1].x && origin[0].y < origin[1].y)
+        let position = [first.frame.origin, second.frame.origin]
+        return position[0].x < position[1].x || (position[0].x == position[1].x && position[0].y < position[1].y)
     }.enumerated().map { (i, screen) in Screen(screen, i) }
 }
---
+++ makefile
@@ -0,0 +1,27 @@
+NAME=hocus
+SRC=src/*.swift
+PLATFORM=x86_64-apple-macosx
+BUILD=.build/$(PLATFORM)/debug
+SUPPORT=SupportFiles
+PACKAGE=$(NAME).app/Contents/MacOS/
+
+install: build $(PACKAGE)
+
+build:
+	swift build
+
+$(PACKAGE):
+	mkdir -p $(PACKAGE) &&\
+	cp $(SUPPORT)/* $(NAME).app/Contents &&\
+	cp $(BUILD)/$(NAME) $(PACKAGE)
+
+run: install
+	open $(NAME).app
+
+clean:
+	rm -rf .buid
+	rm -rf $(NAME).app
+
+re: clean install
+
+.PHONY: install build clean re run
